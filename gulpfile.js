23ef12ab9c118f55bfc18175f75f3e2fb794c8d0
Josh Bernitt <jjbernitt@gmail.com>
2017-03-18 22:09:15 -0400

    Logging in gulp file
    Added logging into gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,15 @@ gulp.task('default', function() {
     return;
 });
 gulp.task('serve', function() {
+    console.log("Serving ./app/server.js...");
     var server = gls.new('./app/server.js');
     server.start();
 
     gulp.watch('./app/server.js', function() {
+        console.log('./app/server.js changed; restarting...');
         server.stop();
         server.start();
+        console.log("server restarted");
     })
 });
 // gulp.task('stop', function() {
