e72cdd01a171eb2881d46fdf1b9a18604ed33453
Josh Bernitt <jjbernitt@gmail.com>
2017-03-19 16:21:22 -0400

    Browser sync and watchers
    Added typescript compilation watchers
    task to gulp so auto compile changed
    ts files and place into public dir. Added
    browser-sync as a gulp task to auto-
    reload the browser whenever public
    files change.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,34 @@
-let gulp    = require('gulp');
-// var express = require('./app/server');
-let gls     = require('gulp-live-server');
-let run     = require('gulp-run');
+const gulp          = require('gulp');
+const gls           = require('gulp-live-server');
+const run           = require('gulp-run');
+const sync          = require('browser-sync').create();
 
-gulp.task('default', function() {
+gulp.task('default', () => {
     console.log("doing nothing...");
     return;
 });
-gulp.task('serve', function() {
+gulp.task('serve', () => {
     console.log("Serving ./app/server.js...");
-    var server = gls.new('./app/server.js');
+    const server = gls.new('./app/server.js');
     server.start();
 
-    gulp.watch('./app/server.js', function() {
+    gulp.watch('./app/server.js', () => {
         console.log('./app/server.js changed; restarting...');
         server.stop();
         server.start();
         console.log("server restarted");
-    })
+    });
 });
-gulp.task('build', function() {
-    return run('npm run build');
-})
+gulp.task('build:ts', () => {
+    console.log("Building typescript sources...");
+    run("npm run build").exec();
+});
+gulp.task('watch:ts', () => {
+    gulp.watch('./src/**/*.ts', ['build:ts']);
+});
+gulp.task('browser-sync', () => {
+    sync.init({
+        proxy: "localhost:3000",
+        files: "./public/**/*"
+    });
+});
